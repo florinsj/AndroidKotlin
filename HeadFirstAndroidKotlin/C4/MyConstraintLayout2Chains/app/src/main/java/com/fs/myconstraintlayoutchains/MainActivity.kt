f4a9aad437516eb49f42150ee2f30dbc2181dbb5
Florin Stanciu <florinspad@gmail.com>
2021-10-02 21:58:57 +0300

    Constraint layout, chain and flow

---
+++ HeadFirstAndroidKotlin/C4/MyConstraintLayout2Chains/app/src/main/java/com/fs/myconstraintlayoutchains/MainActivity.kt
@@ -0,0 +1,11 @@
+package com.fs.myconstraintlayoutchains
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class MainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+    }
+}
