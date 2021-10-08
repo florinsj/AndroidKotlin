65d10509ec5153a5cbc2f6bb07d00b036058935e
Florin Stanciu <florinspad@gmail.com>
2021-10-08 23:56:42 +0300

    when expression Kotlin

--- UdemyCompleteAndroid10Kotlin/Section3/KotlinFundamentals/app/src/main/java/com/fs/kotlinfundamentals/K9_WhenExpression.kt
+++ UdemyCompleteAndroid10Kotlin/Section3/KotlinFundamentals/app/src/main/java/com/fs/kotlinfundamentals/K9_WhenExpression.kt
@@ -19,8 +19,24 @@ fun main() {
         in 6..8 -> println("Summer")
         in 9..11 -> println("Fall")
         12, 1, 2 -> println("Winter")   // if one of the specified values
-        else
-            println
-        ("Invalid Season")
+        else-> println("Invalid Season")
     }
+
+    var age = 25
+    when (age) {
+        in 21..150 -> println("now you may drink in US")
+        in 18..20 -> println("You can vote now")
+        16, 17 -> println("You may drive now")
+        else -> println("you are too young")
+    }
+
+    var x: Any = 133.37
+    when (x) {
+        is Int -> println("$x is an Int")
+        is Double -> println("$x is a Double")
+        is String -> println("$x is a String")
+        else -> println("$x is none of the above")
+
+    }
+
 }
